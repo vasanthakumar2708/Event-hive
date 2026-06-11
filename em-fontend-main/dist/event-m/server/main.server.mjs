a15f55fa6daca722ec7ac4058407df56deecab64
vasanthakumar2708 <vasanthakumarg.27@gmail.com>
2026-06-11 12:38:53 +0530

    Ready for Vercel deployment

--- em-fontend-main/dist/event-m/server/main.server.mjs
+++ em-fontend-main/dist/event-m/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-SAK65ZXU.mjs";import"./chunk-3XSZC7KN.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-EXQITXIC.mjs";import"./chunk-3XSZC7KN.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
