3d2adb116d56f02586c596e7cfc779880eba79a1
vasanthakumar2708 <vasanthakumarg.27@gmail.com>
2026-06-12 14:44:13 +0530

    fixed new vendor field mapping

--- em-backend-main/controller/vendor/newVendor.js
+++ em-backend-main/controller/vendor/newVendor.js
@@ -7,13 +7,14 @@ const newVedor = async (req,res) => {
          console.log("REQUEST BODY:", req.body);
         const newven = new VendorDetails({
             
-            vendor_service:req.body.name,
-            vendor_email:req.body.email,
-            vendor_location:req.body.location,
-            vendor_cname:req.body.cname,
-            vendor_contact:req.body.phone_no,
-            vendor_pincode:req.body.pincode,
-            vendor_password:req.body.password,
+               vendor_service: req.body.vendor_service,
+                vendor_email: req.body.vendor_email,
+                vendor_location: req.body.vendor_location,
+                vendor_cname: req.body.vendor_cname,
+                vendor_contact: req.body.vendor_contact,
+                vendor_pincode: req.body.vendor_pincode,
+                vendor_password: req.body.vendor_password,
+                doj: req.body.doj
             
         })
         console.log(newven);
